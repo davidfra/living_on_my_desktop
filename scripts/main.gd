499ee20585f45831451515efc0d697f9e53fc91c
david <franke.david@googlemail.com>
2024-06-17 17:28:19 +0200

    Finished settings menu

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 enum MenuOpt {
 	ON_TOP,
-	OTHER,
+	SET_POSITION,
 	QUIT
 }
 
@@ -18,27 +18,21 @@ var resolution
 
 @onready var popup_menu = %PopupMenu
 
+@onready var set_margin_panel = %SetMarginPanel
+@onready var margin_spin_box = %MarginSpinBox
+@onready var set_margin_btn = %SetMarginBtn
+@onready var close_margin_btn = %CloseMarginBtn
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_resize_to_fit_screen()
-	popup_menu.add_check_item("Allways on top", MenuOpt.ON_TOP)
-	popup_menu.set_item_checked(MenuOpt.ON_TOP, true)
-	popup_menu.add_item("Other settings", MenuOpt.OTHER)
-	popup_menu.add_item("Exit", MenuOpt.QUIT)
-	popup_menu.id_pressed.connect(_on_popup_item_selected)
+	_build_ui()
 
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_RIGHT:
 		
 		popup_menu.popup(Rect2i(event.global_position.x, 0, 100, get_window().size.y))
-
-func _on_popup_item_selected(id: int):
-	if id == MenuOpt.ON_TOP:
-		var was_checked = popup_menu.is_item_checked(id)
-		get_window().always_on_top = !was_checked
-		popup_menu.set_item_checked(MenuOpt.ON_TOP, !was_checked)
-	elif id == MenuOpt.QUIT:
-		get_tree().quit()
 
 func _resize_to_fit_screen():
 	resolution = DisplayServer.screen_get_size(DisplayServer.window_get_current_screen())
@@ -54,3 +48,30 @@ func _apply_bottom_margin(value):
 	var win = get_window()
 	win.position.y = resolution.y - win.size.y - value
 
+func _build_ui():
+	popup_menu.add_check_item("Always on top", MenuOpt.ON_TOP)
+	popup_menu.set_item_checked(MenuOpt.ON_TOP, true)
+	popup_menu.add_item("Window position ...", MenuOpt.SET_POSITION)
+	popup_menu.add_item("Exit Game", MenuOpt.QUIT)
+	popup_menu.id_pressed.connect(_on_popup_item_selected)
+	
+	# Buttons or SetMarginPanel:
+	set_margin_btn.pressed.connect(_on_set_margin)
+	close_margin_btn.pressed.connect(_on_margin_close)
+	
+func _on_popup_item_selected(id: int):
+	if id == MenuOpt.ON_TOP:
+		var was_checked = popup_menu.is_item_checked(id)
+		get_window().always_on_top = !was_checked
+		popup_menu.set_item_checked(MenuOpt.ON_TOP, !was_checked)
+	elif id == MenuOpt.SET_POSITION:
+		margin_spin_box.value = window_bottom_margin
+		set_margin_panel.show()
+	elif id == MenuOpt.QUIT:
+		get_tree().quit()
+
+func _on_set_margin():
+	window_bottom_margin = margin_spin_box.value
+
+func _on_margin_close():
+	set_margin_panel.hide()
